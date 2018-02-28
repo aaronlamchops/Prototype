9f2305f00fa98db9ee30b86f1a8b84db46e1d282
Aaron Lam <allambchops@gmail.com>
2018-02-28 15:29:30 -0700

    Added navigations to Settings and About Button as Modal's. Updated app.css

--- app/views/main-page/main-page.js
+++ app/views/main-page/main-page.js
@@ -8,3 +8,17 @@ exports.navigate_gamescene = function() {
     var topmost = frameModule.topmost();
     topmost.navigate("views/game-scene/game-scene");
 };
+
+exports.navigate_settings = function(args) {
+    const page = args.object.page;
+    page.showModal("views/settings-screen/settings-screen", "context", function () {
+        console.log("modal opened");
+    }, true);
+};
+
+exports.navigate_about = function(args) {
+    const page = args.object.page;
+    page.showModal("views/about-screen/about-screen", "context", function () {
+        console.log("modal opened");
+    }, true);
+};
